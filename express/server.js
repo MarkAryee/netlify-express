0f42e88726ad6eac3e3239d855696036b9c6053f
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-16 15:50:16 +0000

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,11 +11,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
   //res.writeHead(200, { 'Content-Type': 'text/html' });
  // res.write('<h1>Hellxpress.js!</h1>');
-  res.sendFile(path.join(__dirname, '../express/index.html'));
+ res.sendFile(path.join(__dirname, '../express/index.html'));
  // res.end();
 
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/another', (req, res) => res.json({ router: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
