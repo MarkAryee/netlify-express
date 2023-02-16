0440899c51b7cf69e9f11c98cffa184d6cb6c060
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-16 15:47:31 +0000

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+var createError = require('http-errors');
+
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hellxpress.js!</h1>');
-  res.sendFile(path.join(__dirname, '/express/index.html'));
-  res.end();
+  //res.writeHead(200, { 'Content-Type': 'text/html' });
+ // res.write('<h1>Hellxpress.js!</h1>');
+  res.sendFile(path.join(__dirname, '../express/index.html'));
+ // res.end();
 
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
@@ -19,6 +21,21 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
